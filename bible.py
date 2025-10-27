599faec609fe9be3dbc51c0e4eff46ceb02af75d
LanWifi001 <leanderdylan.bronola@student.laverdad.edu.ph>
2025-10-27 11:07:19 +0800

    Add files via upload

---
+++ bible.py
@@ -0,0 +1,15 @@
+import KJV as k
+
+index = 0
+user = input('Book: ')
+
+for index, value in enumerate(k.bible['books']):
+    if value['name'] == user:
+        for i in range(len(k.bible['books'][index]['chapters'])):
+            book = k.bible['books'][index]['name']
+            chapter = k.bible['books'][index]['chapters'][i]['chapter']
+            for verses in k.bible['books'][index]['chapters'][i]['verses']:
+                verse = verses['verse']
+                print(f"{book} {chapter}:{verse}")
+                print(verses['text'])
+    index+=1
